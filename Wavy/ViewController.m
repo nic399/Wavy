31177c9a7546232647a9d9b9adc7dc9869bde110
Nicholas Fung <nbfung@sfu.ca>
2017-10-19 11:52:11 -0700

    Cleanup code

--- Wavy/ViewController.m
+++ Wavy/ViewController.m
@@ -26,9 +26,6 @@
     
     WavyFlowLayout *myLayout = [[WavyFlowLayout alloc] init];
     self.containerView.collectionViewLayout = myLayout;
-    
-    
-    
 }
 
 
@@ -41,57 +38,11 @@
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     WavyCollectionViewCell *cell = [self.containerView dequeueReusableCellWithReuseIdentifier:@"WavyCell" forIndexPath:indexPath];
     cell.label.text = [NSString stringWithFormat:@"cell%ld", (long)indexPath.item];
-    
     return cell;
 }
 
 - (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    
     return 13;
 }
-
-//- (void)encodeWithCoder:(nonnull NSCoder *)aCoder {
-//    <#code#>
-//}
-//
-//- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
-//    <#code#>
-//}
-//
-//- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
-//    <#code#>
-//}
-//
-//- (CGSize)sizeForChildContentContainer:(nonnull id<UIContentContainer>)container withParentContainerSize:(CGSize)parentSize {
-//    <#code#>
-//}
-//
-//- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
-//    <#code#>
-//}
-//
-//- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
-//    <#code#>
-//}
-//
-//- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
-//    <#code#>
-//}
-//
-//- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
-//    <#code#>
-//}
-//
-//- (void)setNeedsFocusUpdate {
-//    <#code#>
-//}
-//
-//- (BOOL)shouldUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context {
-//    <#code#>
-//}
-//
-//- (void)updateFocusIfNeeded {
-//    <#code#>
-//}
 
 @end
--- Wavy/WavyFlowLayout.m
+++ Wavy/WavyFlowLayout.m
@@ -20,23 +20,13 @@
 
 -(NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
     NSArray *attributesInRect = [[super layoutAttributesForElementsInRect:rect] copy];
-    
-    NSLog(@"There are %ld attributes in the rect", [attributesInRect count]);
-    NSLog(@"Rect dimensions: x:%f, y:%f, width:%f, height:%f", rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
-    NSLog(@"Screen frame: x:%f y:%f width:%f height:%f", self.collectionView.frame.origin.x, self.collectionView.frame.origin.y, self.collectionView.frame.size.width, self.collectionView.frame.size.height);
-    
-    
+
     for (UICollectionViewLayoutAttributes *attr in attributesInRect) {
         CGFloat cellHieght = attr.frame.size.height;
         CGFloat viewHeight = self.collectionView.frame.size.height;
-        NSLog(@"cellHeight: %f, safeYZone: %f", cellHieght, viewHeight);
-        NSLog(@"Center: x:%f y:%f", attr.center.x, attr.center.y);
         attr.center = CGPointMake(attr.center.x, arc4random_uniform(viewHeight-cellHieght)+cellHieght/2.0);
-        attr.size = CGSizeMake(attr.size.width, attr.center.y);
+        attr.size = CGSizeMake(attr.size.width, viewHeight - attr.center.y);
     }
-    
-    
-    
     return attributesInRect;
 }
 
