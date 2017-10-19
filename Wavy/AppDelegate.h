dfa7d8ec7bc63e7a4e055279a4269e98a832c2ff
Nicholas Fung <nbfung@sfu.ca>
2017-10-19 08:54:52 -0700

    Initial Commit

---
+++ Wavy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wavy
+//
+//  Created by Nicholas Fung on 2017-10-19.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wavy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wavy
+//
+//  Created by Nicholas Fung on 2017-10-19.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
